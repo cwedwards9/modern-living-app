48f696558dee147e3f751f1d235d8a92904c985b
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-23 12:42:11 -0500

    Import yelp-fusion library. Add a POST route for handling a search to the yelp API.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+// Yelp Fusion Business API
+const yelp = require("yelp-fusion");
+const apiKey = "YUpzLMMKVaEyh8cYeexDMHxw0DZQGuz8WFJ5xJn2Yq1r8EQocdEV5aC2jH5nQtTHxu0t5RxCCpT5Rq9hOj1JJQ_rWr8r2MquozL5MoCOBRwja53eO5JQkg7OxykDYHYx";
+
+
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
@@ -14,6 +19,19 @@ app.use(express.json());
 app.get("/message", (req, res) => {
     res.json({name: "Chase", message: "Hello!"});
 });
+
+
+app.post("/yelp", (req, res) => {
+    const searchRequest = {...req.body, limit: 5};
+      
+      const client = yelp.client(apiKey);
+      
+      client.search(searchRequest).then(response => { 
+          res.json(response);
+      }).catch(e => {
+        console.log(e);
+      });
+})
 
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
