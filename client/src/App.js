983572503691c28c24290d8f73c2bfc6d51f03df
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-23 13:42:56 -0500

    Update component to render YelpSearch component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Test from "./Test";
+import YelpSearch from "./YelpSearch";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Test />
+        <YelpSearch />
       </div>
     );
   }
