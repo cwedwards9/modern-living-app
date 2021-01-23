a5ef41f848efe3edbc28dc758b3ef5f2c142d2bf
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-23 12:40:10 -0500

    Add a form to submit POST request to our server to get data back from our Yelp API.

--- client/src/YelpSearch.js
+++ client/src/YelpSearch.js
@@ -4,23 +4,61 @@ import axios from "axios";
 class Test extends Component {
     constructor(props) {
         super(props);
-        this.state = { message: ""};
-        this.handleClick = this.handleClick.bind(this);
+        this.state = {businesses: [], term: "", location: ""};
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
-    handleClick() {
-        axios.get("/message")
-            .then((res) => {
-                this.setState({message: res.data.message});
-                console.log(res.data.message);
-            });
+    handleChange(evt) {
+        evt.preventDefault();
+
+        this.setState({
+            [evt.target.name] : evt.target.value
+        });
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+        
+        axios.post("/yelp", {
+            term: this.state.term,
+            location: this.state.location
+        })
+        .then(res => {
+            console.log(res.data.jsonBody.businesses);
+            this.setState({businesses: res.data.jsonBody.businesses});
+        });
     }
     render() {
+        const businesses = this.state.businesses.map(b => (
+            <div key={b.id} style={{marginBottom: "50px"}}>
+                <h1>{b.name}</h1>
+                <p>{b.display_phone}</p>
+                <p>{b.location.display_address[0]}, {b.location.display_address[1]}</p>
+                <p>{b.rating} / 5 Stars</p>
+                <a href={b.url}>See Reviews</a>
+            </div>
+        ));
         return (
-        <div className="App">
-            <h1>This is a test</h1>
-            <button onClick={this.handleClick}>Click</button>
-            <p>{this.state.message}</p>
-        </div>
+            <div>
+                <h1>Search for a Contractor / Designer</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="term">Search Term</label>
+                    <input 
+                        id="term"
+                        value={this.state.term}
+                        name="term"
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="location">Location (city and state)</label>
+                    <input 
+                        id="location"
+                        value={this.state.location}
+                        name="location"
+                        onChange={this.handleChange}
+                    />
+                    <button>Get Businesses</button>
+                </form>
+                {businesses}
+            </div>
         );
     }
 }
