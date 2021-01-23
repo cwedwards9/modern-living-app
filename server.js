f0d3baeb935992b617bbdd6ced568b6adb74b089
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-23 13:44:59 -0500

    Remove /message GET route.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Yelp Fusion Business API
 const yelp = require("yelp-fusion");
-const apiKey = "YUpzLMMKVaEyh8cYeexDMHxw0DZQGuz8WFJ5xJn2Yq1r8EQocdEV5aC2jH5nQtTHxu0t5RxCCpT5Rq9hOj1JJQ_rWr8r2MquozL5MoCOBRwja53eO5JQkg7OxykDYHYx";
+const apiKey = "";
 
 
 const PORT = process.env.PORT || 3001;
@@ -16,17 +16,13 @@ app.use(express.json());
 //     app.use(express.static('build'));
 // }
 
-app.get("/message", (req, res) => {
-    res.json({name: "Chase", message: "Hello!"});
-});
-
 
 app.post("/yelp", (req, res) => {
     const searchRequest = {...req.body, limit: 5};
       
       const client = yelp.client(apiKey);
       
-      client.search(searchRequest).then(response => { 
+      client.search(searchRequest).then(response => {
           res.json(response);
       }).catch(e => {
         console.log(e);
