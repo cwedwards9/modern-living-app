f6b66e53c51a0680446035a46884b7a30db66d52
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-23 13:42:15 -0500

    Change the text input to a dropdown for the search term.

--- client/src/YelpSearch.js
+++ client/src/YelpSearch.js
@@ -8,6 +8,7 @@ class Test extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
+    // On change of a form input, set the state to be the input's value
     handleChange(evt) {
         evt.preventDefault();
 
@@ -15,19 +16,20 @@ class Test extends Component {
             [evt.target.name] : evt.target.value
         });
     }
+    // On submit of the form, send a POST request to '/yelp' to get data from the API
     handleSubmit(evt) {
         evt.preventDefault();
-        
+
         axios.post("/yelp", {
             term: this.state.term,
             location: this.state.location
         })
         .then(res => {
-            console.log(res.data.jsonBody.businesses);
-            this.setState({businesses: res.data.jsonBody.businesses});
+            this.setState({ businesses: res.data.jsonBody.businesses });
         });
     }
     render() {
+        // Iterate through the businesses in the state and create an element for each result with the data
         const businesses = this.state.businesses.map(b => (
             <div key={b.id} style={{marginBottom: "50px"}}>
                 <h1>{b.name}</h1>
@@ -42,12 +44,24 @@ class Test extends Component {
                 <h1>Search for a Contractor / Designer</h1>
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="term">Search Term</label>
-                    <input 
-                        id="term"
-                        value={this.state.term}
-                        name="term"
-                        onChange={this.handleChange}
-                    />
+                    <select id="term" name="term" onChange={this.handleChange}>
+                        <option value="default">--Please choose an option--</option>
+                        <option value="Plumber">Plumber</option>
+                        <option value="Electrician">Electrician</option>
+                        <option value="Carpenter">Carpenter</option>
+                        <option value="Drywaller">Drywaller</option>
+                        <option value="Plastering">Plastering</option>
+                        <option value="Painter">Painter</option>
+                        <option value="Heating and Air-Conditioning">Heating and Air-Conditioning</option>
+                        <option value="Roofer">Roofer</option>
+                        <option value="Landscaper">Landscaper</option>
+                        <option value="Concrete">Concrete</option>
+                        <option value="Flooring">Flooring</option>
+                        <option value="Glass">Glass</option>
+                        <option value="General Contractor">General Contractor</option>
+                        <option value="Designer">Designer</option>
+                    </select>
+
                     <label htmlFor="location">Location (city and state)</label>
                     <input 
                         id="location"
