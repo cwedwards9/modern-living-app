fa2d5a6f58c7b1e850ce1a267d93bc23c84978f1
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-26 15:40:23 -0500

    Implement React router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import YelpSearch from "./YelpSearch";
+import { Route, Switch } from "react-router-dom";
+import YelpSearch from "./pages/YelpSearch";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <YelpSearch />
+        <Navbar />
+        
+        <Switch>
+          <Route exact path="/" render= { () => <Home /> } />
+          <Route exact path="/yelp" render= { () => <YelpSearch /> } />
+        </Switch>
       </div>
     );
   }
