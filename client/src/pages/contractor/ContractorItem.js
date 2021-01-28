0240056dc9db9ecc5a8e9c6a0d227b2ba30b13c6
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-28 11:18:34 -0500

    Render the contractor with relevent info from the api. Add a method, handleSave, to save a single contractor to our db to allow users to refer back to later.

---
+++ client/src/pages/contractor/ContractorItem.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class ContractorItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: "", phone: "", street: "", state: "", rating: "", url: ""};
+        this.handleSave = this.handleSave.bind(this);
+    }
+    handleSave() {
+        const { id, name, phone, street, state, rating, url } = this.props;
+        this.setState({
+            _id: id,
+            name: name,
+            phone: phone,
+            street: street,
+            state: state,
+            rating: rating,
+            url: url
+        }, () => {
+            axios.post("/contractors", this.state)
+        });
+    }
+
+    render() {
+        const { name, phone, street, state, rating, url } = this.props;
+        return (
+            <div className="ContractorItem" style={{marginBottom: "50px"}}>
+                <button onClick={this.handleSave}>Save</button>
+                <h1>{name}</h1>
+                <p>{phone}</p>
+                <p>{street} {state}</p>
+                <p>{rating} / 5 Stars</p>
+                <a href={url}>See Reviews</a>
+            </div>
+        );
+    }
+}
+
+
+export default ContractorItem;
