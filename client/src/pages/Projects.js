189be958fe74ece7290780dbbbb6467deb9d0e9f
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-28 19:08:07 -0500

    Render all of the projects on the page.

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { projects: [] };
+    }
+    componentDidMount() {
+        axios.get("/projects")
+            .then(res => {
+                this.setState({projects: res.data});
+            })
+    }
     render() {
         return (
-            <h1>This is the Projects page</h1>
+            <div className="ProjectsList">
+                {this.state.projects.map(p => (
+                    <div key={p._id}>
+                        <p>{p.title}</p>
+                        <p>{p.category}</p>
+                        <p>{p.estimated_cost}</p>
+                        <p>{p.budget}</p>
+                        <p>{p.notes}</p>
+                    </div>
+                ))}
+            </div>
         );
     }
 }
