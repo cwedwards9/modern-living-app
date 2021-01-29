ca93218feb88d992a7fb5ccfe9b373f83b23f1d0
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-28 19:00:38 -0500

    Add GET route to find all projects in the db and POST route to create a new project.

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,5 +1,21 @@
 const router = require("express").Router();
-const User = require("../models/project");
+const Project = require("../models/project");
+
+// Get all projects
+router.get("/projects", (req, res) => {
+    Project.find({})
+        .then(projects => {
+            res.json(projects);
+        })
+})
+
+// Create a new project
+router.post("/projects", (req, res) => {
+    Project.create(req.body)
+        .then(() => {
+            res.end();
+        })
+});
 
 
 module.exports = router;
