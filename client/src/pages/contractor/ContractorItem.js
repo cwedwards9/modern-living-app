0d675515386e1fab3e9e58b2e48323f493cf7fb1
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-01 13:49:13 -0500

    Remove all properties in state, add one single state property called 'isSaved' which is set to true if a user clicks on the save button. Post the props to the /contractors route instead of the old state.

--- client/src/pages/contractor/ContractorItem.js
+++ client/src/pages/contractor/ContractorItem.js
@@ -4,29 +4,24 @@ import axios from "axios";
 class ContractorItem extends Component {
     constructor(props) {
         super(props);
-        this.state = {name: "", phone: "", locationOne: "", locationTwo: "", rating: "", url: ""};
+        this.state = { isSaved: false };
         this.saveContractor = this.saveContractor.bind(this);
     }
-    // On click of the save button, set the state and send a POST request to '/contractors' with the state
+    // On click of the save button, send a POST request to '/contractors' with the current props
     saveContractor() {
-        const { name, phone, locationOne, locationTwo, rating, url } = this.props;
-        this.setState({
-            name: name,
-            phone: phone,
-            locationOne: locationOne,
-            locationTwo: locationTwo,
-            rating: rating,
-            url: url
-        }, () => {
-            axios.post("/contractors", this.state)
-        });
+        this.setState({ isSaved: true });
+        axios.post("/contractors", this.props)
     }
 
     render() {
         const { name, phone, locationOne, locationTwo, rating, url } = this.props;
         return (
             <div className="ContractorItem" style={{marginBottom: "50px"}}>
-                <button onClick={this.saveContractor}>Save</button>
+                {this.state.isSaved 
+                    ? <button onClick={this.saveContractor}><i className="fas fa-check"></i> Saved</button>
+                    : <button onClick={this.saveContractor}>Save</button>
+                }
+                
                 <h1>{name}</h1>
                 <p>{phone}</p>
                 <p>{locationOne}, {locationTwo}</p>
