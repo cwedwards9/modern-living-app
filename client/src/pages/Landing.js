d77ded176b8e2e7cee8ae33cba790157803bb3f1
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-03 16:36:52 -0500

    Add react-bootstrap components.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,30 +1,28 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
+import './Landing.css'
 
 class Landing extends Component {
     render() {
         return (
-            <div>
-                <h1>Welcome to the Landing page</h1>
-                <section>
-                    <Link to="/inspiration">inspiration</Link>
-                </section>
-                <section>
-                    <Link to="/starter">starter guides</Link>
-                </section>
-                <section>
-                    <Link to="/yelp">Find Help</Link>
-                </section>
-                <section>
-                    <Link to="/projects">projects</Link>
-                </section>
-                <section>
-                    <Link to="/favorites">favorites</Link>
-                </section>
-                <section>
-                    <Link to="/newproject">Start a New Project</Link>
-                </section>
-            </div>
+            <Container>
+                <style>{'body { background-color: #ede9e3; }'}</style>  
+                <h1  className='header'>Welcome</h1>
+                <Row >
+                    <Link className='card' to="/inspiration">inspiration</Link> 
+                    <Link className='card' to="/starter">starter guides</Link>
+                    <Link className='card' to="/yelp">Find Help</Link>
+   
+                </Row>
+                <Row>
+                    <Link className='card' to="/projects">projects</Link>
+                    <Link className='card' to="/favorites">favorites</Link>
+                    <Link className='card' to="/newproject">Start a New Project</Link>
+                </Row>
+            </Container> 
         );
     }
 }
