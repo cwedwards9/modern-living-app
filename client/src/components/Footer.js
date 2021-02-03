892460311f7b75b9da478aaeff8d7b54a0072916
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-03 16:37:54 -0500

    Change background color.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,28 +1,28 @@
 import React from 'react';
+import "./Footer.css";
 import ModernLivingLogo from "../images/ModernLivingLogo650x650White.png";
 
 const footer = () => (
   <div>
     <footer className='container-fluid footer-bg'>
       <div>
-        <div className='row'>
+        <div className='row '>
           <div className='col-md-4'>
             <div className='footer-col-main'>
               <div className='heading-image'>
-                {/* <h2 className='font-weight-bold '>ABOUT US</h2> */}
                 <img src={ModernLivingLogo} className="img-fluid" alt="" />
               </div>
               <div className='content'>
                 <p>Looking to refresh your home? Need ideas on home improvement and don’t know where to start? Modern Living will be your go-to website for all of your home improvement needs.</p>
               </div>
-              <div className='link-call' onClick={() => window.open("mailto:support@modernliving.com", "_self")}>Email us at <span className="email-link">info@modernliving.com</span></div>
+              <div className='link-call' onClick={()=> window.open("mailto:support@modernliving.com", "_self")}>Email us at <span className="email-link">info@modernliving.com</span></div>
             </div>
           </div>
           <div className='col-md-2'>
             <div className='footer-col'>
               <div className='heading'>
-                USEFUL LINKS
-            </div>
+              USEFUL LINKS
+              </div>
               <div className='content'>
                 <div className='link'>Questions</div>
                 <div className='link'>Terms + Conditions</div>
@@ -51,16 +51,16 @@ const footer = () => (
               <div className='content'>
                 <p>Follow us to stay up-to-date on the latest remodeling trends. </p>
                 <div className='socialicon'>
-                  <i className="fab fa-facebook-f"></i>
+                <i className="fab fa-facebook-f"></i>
                 </div>
                 <div className='socialicon'>
-                  <i className="fab fa-linkedin"></i>
+                  <i className="fab fa-twitter"></i>
                 </div>
                 <div className='socialicon'>
-                  <i className="fab fa-linkedin"></i>
+                <i className="fab fa-pinterest"></i>
                 </div>
                 <div className='socialicon'>
-                  <i className="fab fa-instagram"></i>
+                <i className="fab fa-instagram"></i>
                 </div>
               </div>
             </div>
@@ -68,7 +68,6 @@ const footer = () => (
         </div>
       </div>
     </footer>
-
     <div className='subfooter'>
       <div className='row'>
         <div className='col-lg-12'>
@@ -76,7 +75,13 @@ const footer = () => (
         </div>
       </div>
     </div>
-    
+    <div className='subfooter' style={{ color: 'darkgrey', backgroundColor: '#2E2E2E' }}>
+      <div className='row'>
+        <div className='col-lg-12'>
+          <div className='footer-content'>Created by Chase Edwards, Chris Morrow, Nancy Lewis and Ashley Anness.</div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
