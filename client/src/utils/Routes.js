6a29f8771e3ec6889b42ed788ffbd40a8274695a
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-05 17:39:41 -0500

    Remove /favorites route and add /favorites/contractos and /favorites/designs routes rendering their respective components.

--- client/src/utils/Routes.js
+++ client/src/utils/Routes.js
@@ -9,7 +9,8 @@ import Inspiration from "../pages/Inspiration";
 import Starter from "../pages/Starter";
 import ContractorSearch from "../pages/contractor/ContractorSearch";
 import Projects from "../pages/projects/Projects";
-import FavoritesList from "../pages/favorites/FavoritesList";
+import ContractorFavoritesList from "../pages/favorites/ContractorFavoritesList";
+import DesignFavoritesList from "../pages/favorites/DesignFavoritesList";
 import NewProjectForm from "../pages/projects/NewProjectForm";
 import Contact from "../pages/Contact";
 
@@ -19,13 +20,14 @@ class Routes extends Component {
             <Switch>
                 <Route exact path="/" render= { () => <Home /> } />
                 <Route exact path="/register" render={(routeProps)=> <Register routeProps={routeProps} />} />
-                <Route exact path="/Login" render= { () => <Login /> } />
+                <Route exact path="/login" render={(routeProps)=> <Login routeProps={routeProps} />} />
                 <Route exact path="/landing" render= { () => <Landing /> } />
                 <Route exact path="/inspiration" render= { () => <Inspiration /> } />
                 <Route exact path="/starter" render= { () => <Starter /> } />
-                <Route exact path="/yelp" render= { () => <ContractorSearch /> } />
+                <Route exact path="/contractor" render= { () => <ContractorSearch /> } />
                 <Route exact path="/projects" render= { () => <Projects /> } />
-                <Route exact path="/favorites" render= { () => <FavoritesList /> } />
+                <Route exact path="/favorites/contractors" render= { () => <ContractorFavoritesList /> } />
+                <Route exact path="/favorites/designs" render= { () => <DesignFavoritesList /> } />
                 <Route exact path="/newproject" render= { () => <NewProjectForm /> } />
                 <Route exact path="/contact" render= { () => <Contact /> } />
             </Switch>
