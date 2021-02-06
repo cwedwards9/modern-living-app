b45af8c98b07874ed65b3da3e640829d8b807cf0
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-05 19:01:42 -0500

    Import and user card components from react-bootstrap.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,28 +1,31 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-// import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card';
 import './Landing.css'
 
 class Landing extends Component {
     render() {
         return (
-            <Container>
-                <style>{'body { background-color: #ede9e3; }'}</style>  
-                <h1  className='header'>Welcome</h1>
-                <Row >
-                    <Link className='card' to="/inspiration">inspiration</Link> 
-                    <Link className='card' to="/starter">starter guides</Link>
-                    <Link className='card' to="/yelp">Find Help</Link>
-   
-                </Row>
-                <Row>
-                    <Link className='card' to="/projects">projects</Link>
-                    <Link className='card' to="/favorites">favorites</Link>
-                    <Link className='card' to="/newproject">Start a New Project</Link>
-                </Row>
-            </Container> 
+            <div style={ {backgroundColor:"#ede9e3", paddingBottom:"10em",overflow:"hidden"}}>
+                <div className='header'>
+                    <h1 className='h1t' >Welcome,</h1>
+                </div>
+                <div> 
+                    <Container>
+                        <Row>
+                            <Card.Link href="/inspiration" className='landC'> <i className='far fa-lightbulb light' ></i><h2 className='inspoText'>Inspiration </h2></Card.Link>
+                            <Card.Link href="/starter" className='landC'> <i className='far fa-clipboard clip ' ></i><h2 className='guideText'> Starter Guides </h2></Card.Link>
+                            <Card.Link href="/contractor" className='landC'> <i className='fas fa-hands-helping help' ></i><h2 className='findText'> Find Help </h2></Card.Link>
+                        </Row>
+                        <Row> 
+                            <Card.Link href="/projects" className='landC'> <i className='fas fa-tasks check' ></i><h2 className='projectText'> Projects </h2></Card.Link>
+                            <Card.Link href="/favorites/contractors" className='landC'> <i className='far fa-heart fav' ></i><h2 className='favText'> Favorites </h2></Card.Link>
+                            <Card.Link href="/newproject" className='landC'> <i className='fas fa-plus plus' ></i><h2 className='newText'> New Project </h2></Card.Link>
+                        </Row>
+                    </Container> 
+                </div>       
+            </div>
         );
     }
 }
