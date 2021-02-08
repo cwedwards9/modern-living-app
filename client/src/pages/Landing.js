f49db2ec084d4c6db18d6a47a03b50bc31b1ec9e
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-08 11:31:40 -0500

    Import and use CardDeck from react-bootstrap.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,30 +1,53 @@
 import React, { Component } from "react";
+import NavBar from "../components/NavigationBar";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
 import './Landing.css'
 
 class Landing extends Component {
     render() {
         return (
             <div style={ {backgroundColor:"#ede9e3", paddingBottom:"10em",overflow:"hidden"}}>
-                <div className='header'>
-                    <h1 className='h1t' >Welcome,</h1>
-                </div>
-                <div> 
-                    <Container>
-                        <Row>
-                            <Card.Link href="/inspiration" className='landC'> <i className='far fa-lightbulb light' ></i><h2 className='inspoText'>Inspiration </h2></Card.Link>
-                            <Card.Link href="/starter" className='landC'> <i className='far fa-clipboard clip ' ></i><h2 className='guideText'> Starter Guides </h2></Card.Link>
-                            <Card.Link href="/contractor" className='landC'> <i className='fas fa-hands-helping help' ></i><h2 className='findText'> Find Help </h2></Card.Link>
-                        </Row>
-                        <Row> 
-                            <Card.Link href="/projects" className='landC'> <i className='fas fa-tasks check' ></i><h2 className='projectText'> Projects </h2></Card.Link>
-                            <Card.Link href="/favorites/contractors" className='landC'> <i className='far fa-heart fav' ></i><h2 className='favText'> Favorites </h2></Card.Link>
-                            <Card.Link href="/newproject" className='landC'> <i className='fas fa-plus plus' ></i><h2 className='newText'> New Project </h2></Card.Link>
-                        </Row>
-                    </Container> 
-                </div>       
+                <NavBar />
+                
+                <Row>
+                    <div className="col-md-7 px-0">
+                        <div className='header'>
+                            <h1 className='h1t'>LET'S GET STARTED!</h1>
+                        </div>
+                    </div>
+                </Row>
+                <Container>
+                    <CardDeck className="d-flex align-items-stretch" >
+                        <Card className="card-landing  w-100 " >
+                        <Card.Link href="/inspiration"><Card.Title className="card-landing-title text-center"><i className='far fa-lightbulb light' ></i><h3 style={{color: "white"}}>INSPIRATION </h3></Card.Title></Card.Link>
+                        </Card>
+                        <Card className="card-landing w-100 ">
+                        <Card.Link href="/starter"><Card.Title className="card-landing-title text-center"><i className='far fa-clipboard clip ' ></i><h3 style={{color: "white"}}> STARTER GUIDES </h3></Card.Title></Card.Link>
+                        </Card>
+                        <Card className="card-landing w-100 ">
+                        <Card.Link href="/contractor"><Card.Title className="card-landing-title text-center"><i class="fas fa-hands-helping helping"></i><h3 style={{color: "white"}}> FIND HELP </h3></Card.Title></Card.Link>
+                        </Card>
+                    </CardDeck>
+
+                </Container>
+
+                <Container>
+                    <CardDeck className="d-flex align-items-stretch" >
+                        <Card className="card-landing  w-100 " >
+                        <Card.Link href="/projects"><Card.Title className="card-landing-title text-center"><i className='far fa-heart fav' ></i><h3 style={{color: "white"}}>FAVORITES </h3></Card.Title></Card.Link>
+                        </Card>
+                        <Card className="card-landing w-100 ">
+                        <Card.Link href="/favorites/contractors"><Card.Title className="card-landing-title text-center"><i className='fas fa-tasks check' ></i><h3 style={{color: "white"}}> PROJECTS</h3></Card.Title></Card.Link>
+                        </Card>
+                        <Card className="card-landing w-100 ">
+                        <Card.Link href="/newproject"><Card.Title className="card-landing-title text-center"><i className='fas fa-plus plus' ></i><h3 style={{color: "white"}}> NEW PROJECT</h3></Card.Title></Card.Link>
+                        </Card>
+                    </CardDeck>
+
+                </Container>
             </div>
         );
     }
