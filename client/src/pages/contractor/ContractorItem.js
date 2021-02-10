69bb251870340413ee2d48c6d55beb6fcb5978f5
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-10 11:48:50 -0500

    Add styling/structure to the search form, add image.

--- client/src/pages/contractor/ContractorItem.js
+++ client/src/pages/contractor/ContractorItem.js
@@ -10,26 +10,25 @@ class ContractorItem extends Component {
     // On click of the save button, send a POST request to '/contractors' with the current props
     saveContractor() {
         this.setState({ isSaved: true });
-        axios.post("/contractors", this.props)
+        axios.post("/api/contractors", this.props)
     }
 
     render() {
         const { name, phone, locationOne, locationTwo, rating, url } = this.props;
         return (
-            <div className="ContractorItem col-lg-6 flex">
-                <i className='yelp fab fa-yelp'></i>
-                {this.state.isSaved 
-                    ? <button onClick={this.saveContractor}><i style={{color: "red"}} className='heart fas fa-heart'></i></button>
-                    : <button onClick={this.saveContractor}><i className='heart fas fa-heart'></i></button>
-                }
-                
-                <br></br>
+            <div className="ContractorItem col-lg-6 mb-0">
+                {/* <i className='yelp fab fa-yelp'></i> */}
                 <div className='text' > 
                     <h2 className='cp'>{name}</h2>
                     <p className='cp'>{phone}</p>
                     <p className='cp'>{locationOne}, {locationTwo}</p>
-                    <i className='cp fas fa-star'>{rating} / 5 Stars </i>
-                    <a className='cp' href={url}>See Reviews</a></div>
+                    <i className='cp fas fa-star'>{rating} / 5 STARS </i>
+                    <a className='cp cp-link' href={url}>SEE REVIEWS</a>
+                    {this.state.isSaved 
+                        ? <button onClick={this.saveContractor}><i style={{color: "red"}} className='heart fas fa-heart'></i></button>
+                        : <button onClick={this.saveContractor}><i className='heart fas fa-heart'></i></button>
+                    }
+                </div>    
             </div>
         ); 
     }
