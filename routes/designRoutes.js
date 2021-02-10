4d00d69cc5388755ffbb2e818d6cb9bab4785099
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-10 11:44:36 -0500

    On each POST, PUT, and DELETE route, add that new document to the user that created it. On each GET request, find the user that is making the request in the db and load the appropriate data (projects, designs, contractors) of that user.

--- routes/designRoutes.js
+++ routes/designRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Design = require("../models/design");
+const db = require("../models");
 // Unsplash images API
 const { createApi } = require("unsplash-js");
 const nodeFetch = require("node-fetch");
@@ -11,47 +11,57 @@ const unsplash = createApi({ accessKey: accessKey, fetch: nodeFetch });
 
 // GET route for getting Unsplash API data
 router.get("/unsplash", (req, res) => {
-    unsplash.search.getPhotos({...req.query, perPage: 12})
-        .then(result => {
-            if (result.errors) {
-                // handle error here
-                console.log('error occurred: ', result.errors[0]);
-                res.json("failed");
-            } else {
-                // handle success here
-                res.json(result.response);
-            }
-        });
+    unsplash.search.getPhotos({...req.query, perPage: 12, orientation: "landscape"})
+    .then(result => {
+        if (result.errors) {
+            // handle error here
+            console.log('error occurred: ', result.errors[0]);
+            res.json("failed");
+        } else {
+            // handle success here
+            res.json(result.response);
+        }
+    });
 });
 
 
 
 // Show all designs the user saved in favorites
 router.get("/designs", (req, res) => {
-    Design.find({})
-        .then(contractors => {
-            res.json(contractors);
-        })
+    db.User.findById(req.user._id)
+    .populate("designs")
+    .then(foundUser => {
+        res.json(foundUser.designs);
+    })
 });
   
 
 
 // Add/Save a new designs to the favorites
 router.post("/designs", (req, res) => {
-    Design.create(req.body)
-        .then(() => {
+    db.User.findById(req.user._id)
+    .then((foundUser) => {
+        db.Design.create({...req.body, userid: req.user._id})
+        .then(newDesign => {
+            newDesign.save();
+            foundUser.designs.push(newDesign._id);
+            foundUser.save();
             res.end();
         })
+    })
 });
   
 
 
   // Delete a designs from favorites
 router.delete("/designs/:id", (req, res) => {
-    Design.findByIdAndDelete(req.params.id)
+    db.Design.findByIdAndDelete(req.params.id)
+    .then(() => {
+        db.User.updateOne({_id: req.user._id}, { $pull: { designs: req.params.id }})
         .then(() => {
             res.end();
         })
+    })
 });
 
 module.exports = router;
