0d61a54aa78e0a85c1af1759709c34c8d37da013
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-11 12:11:15 -0500

    Pass the results, from the API search, down to the new InspirationItem component.

--- client/src/pages/Inspiration.js
+++ client/src/pages/Inspiration.js
@@ -1,20 +1,10 @@
 import React, { Component } from "react";
+import InspirationItem from "./InspirationItem";
 import "./Inspiration.css";
 import axios from "axios";
 import NavBar from '../components/NavigationBar';
 import { Container, Row } from "reactstrap";
-import { Card, CardDeck, Button } from 'react-bootstrap';
 import Divider from '@material-ui/core/Divider';
-
-import Kitchen400 from "../images/Kitchen400x400.jpg";
-import Bathroom400 from "../images/Bathroom400x400.jpg";
-import Media400 from "../images/Media400x400.jpg";
-import Living400 from "../images/Living400x400.jpg";
-
-const cardRooms = [ 
-    {id: 1, room: "KITCHEN", image: Kitchen400}, {id: 2, room: "BATHROOM", image: Bathroom400}, 
-    {id: 3, room: "MEDIA ROOM", image: Media400}, {id: 4, room: "LIVING ROOM", image: Living400} 
-];
 
 
 class Inspiration extends Component {
@@ -34,26 +24,25 @@ class Inspiration extends Component {
     handleSubmit(evt) {
         evt.preventDefault();
 
-        axios.get("/unsplash", {
+        axios.get("/api/unsplash", {
             params: {
                 query: this.state.searchTerm
             }
         })
         .then(res => {
-            console.log(res.data.results);
             this.setState({ designs: res.data.results });
         })
     }
     saveDesign(desc, url) {
-        axios.post("/designs", { description: desc, url: url });
+        axios.post("/api/designs", { description: desc, url: url });
     }
 
     render() {
         return (
-            <div className="Inspiration">
+            <div className="Inspiration" style={{ backgroundColor: '#ede9e3' }}>
                 <NavBar />
-                <div>
-                    <Container fluid style={{ backgroundColor: '#ede9e3' }}>
+                <div style={{ backgroundColor: '#ede9e3' }}>
+                    <Container fluid >
                         <div>
                             <Row className="inspiration-title">
                                 <span className="inspiration-header1">SOME</span>
@@ -66,44 +55,29 @@ class Inspiration extends Component {
                         <div className="InspirationSearch">
                             <p>We want to assist you by finding the best designs for your next project.</p>
                             <form onSubmit={this.handleSubmit}>
-                                <input 
+                                <input
                                     name="searchTerm"
                                     value={this.state.searchTerm}
                                     type="text"
                                     onChange={this.handleChange}
                                 />
-                                <button>Search</button>
+                                <button className="inspo-search-btn">SEARCH</button>
                             </form>
                         </div>
-                    </Container>
-
-                    <Container className="inspiration-cards">
-                        <CardDeck className="d-flex">
-                            {cardRooms.map(c => (
-                                <Card className="d-flex align-items-stretch w-100">
-                                    <Card.Img variant="top" src={c.image} />
-                                    <Button className="inspiration-button">
-                                        <Card.ImgOverlay className={`inspiration-img-overlay${c.id}`} >
-                                            <Card.Title className="inspiration-card-title text-center"><span className="inspiration-title-text">{c.room}</span></Card.Title>
-                                        </Card.ImgOverlay>
-                                    </Button>
-                                </Card>
-                            ))}
-                        </CardDeck>
                     </Container>
                 </div>
 
                 {/* Results */}
-                <Container>
+                <Container className="inspiration-results" style={{ backgroundColor: '#ede9e3' }}>
                     <div className="row unsplash-image-grid d-flex">
-                    {this.state.designs.map(d => (
-                        <div className="col-sm-3 col-md-3" key={d.id}>
-                            <div className="inspiration-image-unsplash"><a href target="_self">
-                                <img src={d.urls.full} alt={d.alt_description} className="img-responsive center-block" />
-                                </a>
-                                <button onClick={() => {this.saveDesign(d.alt_description, d.urls.full)} }><i class="far fa-heart fa-heart-inspiration"></i></button>
-                            </div>
-                        </div>
+                        {this.state.designs.map(d => (
+                            <InspirationItem 
+                                className="col-sm-4 col-md-4" 
+                                key={d.id}
+                                url={d.urls.full}
+                                desc={d.alt_description}
+                                saveDesign={this.saveDesign}
+                            />
                         ))}
                     </div>
                 </Container>
