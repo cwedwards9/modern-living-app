4698e6a127c8a1518d468c7d1bef850f917fe185
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-11 11:15:45 -0500

    Add a Redirect route as the very last route in the switch.

--- client/src/utils/Routes.js
+++ client/src/utils/Routes.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import ProtectedRoute from "./ProtectedRoutes";
 
 import Home from "../pages/Home";
 import Register from "../pages/Register";
@@ -14,29 +16,27 @@ import DesignFavoritesList from "../pages/favorites/DesignFavoritesList";
 import NewProjectForm from "../pages/projects/NewProjectForm";
 import Contact from "../pages/Contact";
 
-import ProtectedRoute from "./ProtectedRoutes";
-
 
 class Routes extends Component {
-    
     render() {
         // Check user session to see if user is logged in (session created on login/register)
         const user = sessionStorage.getItem("user");
         return (
-            // Protected Routes are only accessible if there is a user in the session storage 
+            // Protected Routes are accessible if a 'user' is in the session storage
             <Switch>
                 <Route exact path="/" render= { () => <Home /> } />
                 <Route exact path="/register" render={(routeProps)=> <Register routeProps={routeProps} />} />
                 <Route exact path="/login" render={(routeProps)=> <Login routeProps={routeProps} />} />
                 <Route exact path="/contact" render= { () => <Contact /> } />
-                <Route path="/landing" render= {() => <Landing />}  />
-                <ProtectedRoute path="/inspiration" component={Inspiration} isAuth={user} />
+                <Route exact path="/landing" render= {() => <Landing />}  />
+                <ProtectedRoute exact path="/inspiration" component={Inspiration} isAuth={user} />
                 <ProtectedRoute exact path="/starter" component= {Starter} isAuth={user} />
                 <ProtectedRoute exact path="/contractor" component= {ContractorSearch} isAuth={user} />
                 <ProtectedRoute exact path="/projects" component= {Projects} isAuth={user} />
                 <ProtectedRoute exact path="/favorites/contractors" component= {ContractorFavoritesList} isAuth={user} />
                 <ProtectedRoute exact path="/favorites/designs" component= { DesignFavoritesList} isAuth={user} />
                 <ProtectedRoute exact path="/newproject" component= {NewProjectForm} isAuth={user} />
+                <Redirect to="/" />
             </Switch>
         );
     }
