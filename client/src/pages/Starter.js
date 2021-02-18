ef4a7c5d8654d5d2e2a70af31100a6c7b69f035a
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-18 10:44:56 -0500

    Restructure the page header section and the starter cards section, removing bootstrap elements, replacing with flexbox and other styles.

--- client/src/pages/Starter.js
+++ client/src/pages/Starter.js
@@ -1,12 +1,8 @@
 import NavBar from "../components/NavigationBar";
-import { Row } from "reactstrap";
-import { Card, Button } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Card } from "react-bootstrap";
 import "./Starter.css";
 import { useState } from "react";
 import Accordion from 'react-bootstrap/Accordion';
-import Divider from '@material-ui/core/Divider';
-import Contractor from "../images/Contractor.png";
 
 import roomData from "../utils/StarterPageData";
 
@@ -26,63 +22,49 @@ function Starter() {
     return (
         <>
             <NavBar />
-            <div className="starter-body" style={{ backgroundColor: '#ede9e3' }}>
-                <div>
-                    <Row className="inspiration-title">
-                        <span className="inspiration-header1">STARTER</span>
-                    </Row>
-                    <Row className="inspiration-title2">
-                        <span className="inspiration-header2">GUIDES<Divider /></span>
-                    </Row>
-                </div>
-                <div className="starter-flex-box">
-                    {data.map((eachCategory, indexCategory) => {
-                        return (
-                            <div key={indexCategory}>
-                                <div className="starter-cards-1" style={{ backgroundColor: eachCategory.colors }} onClick={() => handleCategoryClick(indexCategory)} >
-                                    <div className="starter-card-icons" >{eachCategory.icon}</div>
-                                        <div className="starter-card-text" >{eachCategory.name}</div>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
+            <main className="starter-body">
+                <section className="starter-header">
+                    <h1>STARTER<span style = {{display: "block"}}>GUIDES</span></h1>
+                </section>
 
-                <div className="accord-1">
-                    {data.map((eachCategory, indexCategory) => {
-                        return (
-                            <div className="accord-2" key={indexCategory}>
-                                {eachCategory.showDropdowns &&
-                                    <div className="accord-3">
-                                        {eachCategory.dropdowns.map((eachDropdown, indexDropdown) => {
-                                            return (
-                                                <div className="starter-box" key={indexDropdown}>
-                                                    <Accordion className="main-accord">
-                                                        <Card className="accord">
-                                                            <Accordion.Toggle as={Card.Header} eventKey="0">
-                                                                <div className="main-dropdown-title">{eachDropdown.name}<i className="fas fa-sort-down"></i></div>
-                                                            </Accordion.Toggle>
-                                                            <Accordion.Collapse eventKey="0">
-                                                                <Card.Body>
-                                                                    {eachDropdown.options.map((eachOptions, indexOptions) => {
-                                                                        return (
-                                                                            <option value="" key={indexOptions}>{eachOptions}</option>
-                                                                        )
-                                                                    })}
-                                                                </Card.Body>
-                                                            </Accordion.Collapse>
-                                                        </Card>
-                                                    </Accordion>
-                                                </div>
-                                            )
-                                        })}
-                                    </div>
-                                }
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
+                <section className="starter-cards-section">
+                    {data.map((eachCategory, indexCategory) => (
+                        <div className="starter-cards" key={indexCategory} style={{ backgroundColor: eachCategory.colors }} onClick={() => handleCategoryClick(indexCategory)} >
+                            {eachCategory.icon}
+                            <div className="starter-card-text" >{eachCategory.name}</div>
+                        </div>
+                    ))}
+                </section>
+
+                <section>
+                    {data.map((eachCategory, indexCategory) => (
+                        <div key={indexCategory}>
+                            {eachCategory.showDropdowns &&
+                                <div>
+                                    {eachCategory.dropdowns.map((eachDropdown, indexDropdown) => (
+                                        <div className="starter-box" key={indexDropdown}>
+                                            <Accordion className="main-accord">
+                                                <Card className="accord">
+                                                    <Accordion.Toggle as={Card.Header} eventKey="0">
+                                                        <div className="main-dropdown-title">{eachDropdown.name}<i className="fas fa-sort-down"></i></div>
+                                                    </Accordion.Toggle>
+                                                    <Accordion.Collapse eventKey="0">
+                                                        <Card.Body>
+                                                            {eachDropdown.options.map((eachOptions, indexOptions) => (    
+                                                                <option value="" key={indexOptions}>{eachOptions}</option>
+                                                            ))}
+                                                        </Card.Body>
+                                                    </Accordion.Collapse>
+                                                </Card>
+                                            </Accordion>
+                                        </div>
+                                    ))}
+                                </div>
+                            }
+                        </div>
+                    ))}
+                </section>
+            </main>
         </>
     );
 }
